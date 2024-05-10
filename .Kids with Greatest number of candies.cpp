4c03048808306fc4c28b319522b6bb5a29b62f8c
Riya Jaiswal <152481938+riya677@users.noreply.github.com>
2024-05-10 20:32:29 +0530

    Add files via upload

---
+++ .Kids with Greatest number of candies.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int maxCandies = *max_element(candies.begin(), candies.end());
+        vector<bool> result;
+
+        for (int i=0; i < candies.size(); ++i) {
+            result.push_back(candies[i] + extraCandies >= maxCandies);
+        }
+
+        return result;
+        
+    }
+};
