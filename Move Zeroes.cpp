cf415d14bdabca710900eadd17762586bff36e8e
Riya Jaiswal <152481938+riya677@users.noreply.github.com>
2024-05-12 22:54:25 +0530

    Add files via upload

---
+++ Move Zeroes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int left = 0;
+
+        for(int i = 0; i <n ; i++){
+            if (nums[i] !=0) {
+            nums[left++] = nums[i];
+            }
+        }
+        while (left < n) {
+            nums[left++] =0;
+        }
+    }
+
+};
